c3a5afe3938b9b98bdbfbfbe9b8255169e758126
jiaqifei <jiaqifei@snboxsoft.com>
2019-03-07 13:23:58 +0800

    RabbtMQ 发送数据 消费数据小工具1

--- MQTools/MQTools/RabbitMQHelper.cs
+++ MQTools/MQTools/RabbitMQHelper.cs
@@ -6,6 +6,9 @@ namespace Rabbit.MQTools
 {
     public class RabbitMQHelper
     {
+
+        //queueDeclare(String queue,boolean durable,boolean exclusive, Map<String, Object> arguments);
+        //durable： 是否持久化  exclusive：是否排外的  autoDelete：是否自动删除
         /// <summary>
         /// 向MQ发送数据
         /// </summary>
@@ -20,20 +23,48 @@ namespace Rabbit.MQTools
                 factory.Port = Convert.ToInt32(model.Port);// 5672;// Constants.MqPort;
                 factory.UserName = model.userName;//Constants.MqUserName;
                 factory.Password = model.pwd;// Constants.MqPwd;
-                using (IConnection conn = factory.CreateConnection())
+                try
                 {
-                    using (IModel channel = conn.CreateModel())
+                    using (IConnection conn = factory.CreateConnection())
                     {
-                        //在MQ上定义一个持久化队列，如果名称相同不会重复创建
-                        channel.QueueDeclare(model.ToPic, true, false, false, null);
-                        byte[] bytes = Encoding.UTF8.GetBytes(model.content);
-                        //设置消息持久化
-                        IBasicProperties properties = channel.CreateBasicProperties();
-                        properties.DeliveryMode = 2;
-                        channel.BasicPublish("", model.ToPic, properties, bytes);
+                        using (IModel channel = conn.CreateModel())
+                        {
+                            //在MQ上定义一个持久化队列，如果名称相同不会重复创建
 
-                        return string.Format("[{0}]消息已发送：{1}", model.ToPic, model.content);
+                            channel.QueueDeclare(model.ToPic, true, false, false, null);
 
+                            byte[] bytes = Encoding.UTF8.GetBytes(model.content);
+                            //设置消息持久化
+                            IBasicProperties properties = channel.CreateBasicProperties();
+                            properties.DeliveryMode = 2;
+                            channel.BasicPublish("", model.ToPic, properties, bytes);
+
+                            return string.Format("[{0}]消息已发送：{1}", model.ToPic, model.content);
+
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    //The AMQP operation was interrupted: AMQP close-reason, initiated by Peer, code=406, text="PRECONDITI
+                    using (IConnection conn = factory.CreateConnection())
+                    {
+                        using (IModel channel = conn.CreateModel())
+                        {
+                            //在MQ上定义一个持久化队列，如果名称相同不会重复创建
+
+                            //channel.ExchangeDeclare("World", "direct");
+                            channel.QueueDeclare(model.ToPic, false, false, false, null);
+
+                            byte[] bytes = Encoding.UTF8.GetBytes(model.content);
+                            //设置消息持久化
+                            IBasicProperties properties = channel.CreateBasicProperties();
+                            properties.DeliveryMode = 2;
+                            channel.BasicPublish("", model.ToPic, properties, bytes);
+
+                            return string.Format("[{0}]消息已发送：{1}", model.ToPic, model.content);
+
+                        }
                     }
                 }
             }
@@ -43,6 +74,6 @@ namespace Rabbit.MQTools
             }
         }
 
-        
+
     }
 }
